580339ab60b4cf42a9ea2688cc8d1641c4028bb3
k98940 <pv1xxb@yandex.ru>
2023-10-12 16:00:18 +0600

    feat: the task is ready. (Thu, Oct 12, 2023 04:00:18 PM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []
+  try {
+    [...str].forEach((bracket, i) => {
+      bracketsConfig.forEach((config) => {
+        const openBracket = config[0]
+        const closeBracket = config[1]
+        const lastInStack = stack.length - 1
+        const isCloseBracket = bracket === closeBracket
+        const isCorrectOpenBracket = stack[lastInStack] === openBracket
+        const isSameBrackets = stack[lastInStack] === closeBracket
+
+        if (bracket === openBracket) {
+          if (isSameBrackets) {
+            stack.pop()
+          } else {
+            stack.push(bracket)
+          }
+        } else if (isCloseBracket) {
+          if (stack.length === 0) {
+            throw new Error()
+          }
+          if (isCorrectOpenBracket) {
+            stack.pop()
+          }
+        }
+
+      })
+    }
+    )
+  } catch (error) {
+    return false
+  }
+
+  return stack.length === 0
 }
